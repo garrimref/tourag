eeff9ce663c0cb9bdd34545af0d8419fd9e1824c
garrimref <98973113+garrimref@users.noreply.github.com>
2024-05-26 21:33:13 +0200

    init

---
+++ src/main/java/com/reltour/tourag/TouragApplication.java
@@ -0,0 +1,13 @@
+package com.reltour.tourag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouragApplication.class, args);
+	}
+
+}
